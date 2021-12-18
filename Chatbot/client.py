fce2740126b71e16d9547b1dc0d31eb2b58727ed
OmarMohamed156 <om658036@gmail.com>
2021-12-18 18:35:54 +0200

    chatbot finished

---
+++ Chatbot/client.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+import sys
+import json
+import socket
+from PyQt5 import QtWidgets
+from socket_chatbot import Ui_MainWindow
+
+
+# setting up the HOST ip & PORT number from the HOST_PORT.json file
+with open('HOST_PORT.json', 'r', encoding='utf-8') as f:
+    HOST_PORT = json.load(f)
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        # attaching the gui to our client
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        # appending a welcoming message when the program first runs
+        self.ui.textBrowser.append('<font color="#FF0000">Bot : I am ready for your Questions doctor. Go ahead ask me any thing You want</font>')
+        #setting the client port and host of the client as those inside the json file
+        self.HOST = HOST_PORT['HOST']
+        self.PORT = int(HOST_PORT['PORT'])
+        # attaching the button click event to a connection function responsible for sending data to server
+        self.ui.pushButton.clicked.connect(self.buttonEvent)
+
+    def buttonEvent(self):
+        # appending the text hte user sends through the textEdit widget to the textBrowser widget with the "you" key word preciding it 
+        text = self.ui.textEdit.toPlainText()
+        self.ui.textEdit.clear()
+        self.ui.textBrowser.append('You: '+text)
+        # using tcp socet connection from socket library (AF_INET means it utilizes internet connection)
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # connecting to the host ip and port provided by the json file
+        self.client.connect((self.HOST, self.PORT))
+        # sending the encoded user message
+        self.client.sendall(text.encode())
+
+        # Recieving server message (not more than 1024 bytes with encoding type of utf-8)
+        serverMessage = str(self.client.recv(1024), encoding='utf-8')
+        # appending the server response to the textBrowser widget in the GUI
+        self.ui.textBrowser.append('<font color="#FF0000">Bot : '+serverMessage+'</font>')
+        # closing the connection
+        self.client.close()
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication([])
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
---
+++ Chatbot/socket_chatbot.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'chatbot.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(811, 596)
+        MainWindow.setStyleSheet("")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser.setGeometry(QtCore.QRect(10, 20, 781, 431))
+        self.textBrowser.setStyleSheet("background-color: rgb(238, 238, 236);\n"
+"border-style: outset;\n"
+"border-width: 2px;\n"
+"border-radius: 15px;\n"
+"padding: 4px;")
+        self.textBrowser.setObjectName("textBrowser")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(578, 474, 211, 61))
+        self.pushButton.setStyleSheet("background-color: rgb(237, 212, 0);")
+        self.pushButton.setObjectName("pushButton")
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(20, 470, 541, 70))
+        self.textEdit.setStyleSheet("border-style: outset;\n"
+"border-width: 2px;\n"
+"border-radius: 15px;\n"
+"padding: 4px;\n"
+"background-color: rgb(238, 238, 236);")
+        self.textEdit.setObjectName("textEdit")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 811, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Send"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
