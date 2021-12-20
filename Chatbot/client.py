f35f5fd79a0fc561b1f0c4805196a72b646ccf72
Ramadan Ibrahem <61190974+RamadanIbrahem98@users.noreply.github.com>
2021-12-20 15:02:09 +0200

    fix: UI size constraints

--- Chatbot/client.py
+++ Chatbot/client.py
@@ -3,7 +3,7 @@ import sys
 import json
 import socket
 from PyQt5 import QtWidgets
-from socket_chatbot import Ui_MainWindow
+from GUI import Ui_MainWindow
 
 
 # setting up the HOST ip & PORT number from the HOST_PORT.json file
@@ -18,29 +18,37 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         # appending a welcoming message when the program first runs
-        self.ui.textBrowser.append('<font color="#FF0000">Bot : I am ready for your Questions doctor. Go ahead ask me any thing You want</font>')
-        #setting the client port and host of the client as those inside the json file
+        self.ui.textBrowser.append(
+            '<font color="#FF0000">Bot : I am ready for your Questions doctor. Go ahead ask me any thing You want</font>')
+        # setting the client port and host of the client as those inside the json file
         self.HOST = HOST_PORT['HOST']
         self.PORT = int(HOST_PORT['PORT'])
         # attaching the button click event to a connection function responsible for sending data to server
         self.ui.pushButton.clicked.connect(self.buttonEvent)
 
     def buttonEvent(self):
-        # appending the text hte user sends through the textEdit widget to the textBrowser widget with the "you" key word preciding it 
+        # appending the text hte user sends through the textEdit widget to the textBrowser widget with the "you" key word preciding it
         text = self.ui.textEdit.toPlainText()
         self.ui.textEdit.clear()
         self.ui.textBrowser.append('You: '+text)
         # using tcp socet connection from socket library (AF_INET means it utilizes internet connection)
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # connecting to the host ip and port provided by the json file
-        self.client.connect((self.HOST, self.PORT))
+        try:
+            self.client.connect((self.HOST, self.PORT))
+        except ConnectionRefusedError as e:
+            self.ui.textBrowser.append(
+                '<font color="#FF0000">500: Internal Server Error</font>')
+            self.client.close()
+            return ''
         # sending the encoded user message
         self.client.sendall(text.encode())
 
         # Recieving server message (not more than 1024 bytes with encoding type of utf-8)
         serverMessage = str(self.client.recv(1024), encoding='utf-8')
         # appending the server response to the textBrowser widget in the GUI
-        self.ui.textBrowser.append('<font color="#FF0000">Bot : '+serverMessage+'</font>')
+        self.ui.textBrowser.append(
+            '<font color="#FF0000">Bot : '+serverMessage+'</font>')
         # closing the connection
         self.client.close()
 
